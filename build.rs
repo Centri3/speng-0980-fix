d75cd7b7c9fa943e4d9630de01020db3e4febd35
Catherine Flores <catherine.3.flores@gmail.com>
2023-07-20 10:27:59 -0500

    go

---
+++ build.rs
@@ -0,0 +1,20 @@
+use std::env::var;
+use std::path::Path;
+
+fn main() {
+    println!("cargo:rustc-link-search=./include/bin");
+    if var("TARGET").unwrap().starts_with("x86_64") {
+        println!("cargo:rustc-link-lib=static=nvapi64");
+    } else {
+        println!("cargo:rustc-link-lib=static=nvapi");
+    }
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    bindgen::builder()
+        .header("wrapper.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings")
+        .write_to_file(Path::new(&var("OUT_DIR").unwrap()).join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
